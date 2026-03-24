c060fb8d8dc7c45951154300fca0b8943924ad48
mhlinskaia <mhlinskaia@dataart.com>
2026-03-24 16:56:19 +0100

    fixed tests

--- react-zustand-app/src/pages/start/StartPage.tsx
+++ react-zustand-app/src/pages/start/StartPage.tsx
@@ -1,8 +1,16 @@
+import { Box } from "@mui/material";
+
 export default function StartPage() {
   return (
-    <>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      gap={2}
+      mt={4}
+    >
       <h1>Welcome!</h1>
       <p>Where would you like to go next?</p>
-    </>
+    </Box>
   );
 }
--- react-zustand-app/src/pages/start/tests/StartPage.test.tsx
+++ react-zustand-app/src/pages/start/tests/StartPage.test.tsx
@@ -2,15 +2,43 @@ import { MemoryRouter } from "react-router-dom";
 import StartPage from "../StartPage";
 import { render, screen } from "@testing-library/react";
 
+const renderStartPage = () =>
+  render(
+    <MemoryRouter>
+      <StartPage />
+    </MemoryRouter>,
+  );
+
 describe("StartPage", () => {
   it("should render welcome message", () => {
-    render(
-      <MemoryRouter>
-        <StartPage />
-      </MemoryRouter>,
-    );
+    renderStartPage();
+
     expect(
-      screen.getByText("Welcome! Where would you like to go next?"),
+      screen.getByRole("heading", { level: 1, name: "Welcome!" }), // find <h1>Welcome!</h1>
     ).toBeInTheDocument();
+  });
+
+  it("should render main description message", () => {
+    renderStartPage();
+
+    const paragraph = screen.getByText(/where would you like to go next\?/i);
+
+    expect(paragraph.tagName).toBe("P"); // find <p> with this text
+  });
+
+  it("render navigation list", () => {
+    renderStartPage();
+
+    const navigationList = screen.getByRole("list");
+
+    expect(navigationList).toBeInTheDocument();
+  });
+
+  it("render navigation list items", () => {
+    renderStartPage();
+
+    const navigationItems = screen.getAllByRole("listitem");
+
+    expect(navigationItems).toHaveLength(2);
   });
 });
